9c462052e2b769f99b6f3e74d220561a0080c52a
Nazar <bonasane@gmail.com>
2024-05-18 12:21:15 +0300

    Урок1

--- weather.py
+++ weather.py
@@ -2,14 +2,17 @@ from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 from kivy.lang import Builder
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.backdrop.backdrop import MDCard
 
-class MainScreen(MDScreen):
+class WeatherCard(MDCard):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 
-
+class MainScreen(MDScreen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
 class WeatherApp(MDApp):
