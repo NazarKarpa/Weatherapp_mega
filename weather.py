ad62fcaa71921709b6b7a9b8575e6cb973091202
Nazar <bonasane@gmail.com>
2024-05-25 12:29:46 +0300

    Урок3

--- weather.py
+++ weather.py
@@ -4,6 +4,11 @@ from kivy.lang import Builder
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.backdrop.backdrop import MDCard
 
+import config
+from config import APY_KEY, API_URL
+import requests as req
+
+
 class WeatherCard(MDCard):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -13,6 +18,23 @@ class WeatherCard(MDCard):
 class MainScreen(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+    def weather_search(self):
+        #Отримуємо текст з кествого поля міста
+        city = self.ids.text_fell.text.strip().lower()
+        #Робимо налажтування
+        api_params = {
+            'q': city,
+            'appid': APY_KEY
+
+        }
+
+        #Робимо запит і получаємо відповідь
+        data = req.get(API_URL, api_params)
+        response = data.json()
+        print(response)
+
+        descripition = response['weather'][0]['description']
+        self.ids.weather_card.ids.label.text = descripition#Звертаємось до карточки і переіменовуємо текст
 
 
 class WeatherApp(MDApp):
