8bb6b01dc90bd3abf6ee333d6f8d1a4a71bb1afa
Nazar <bonasane@gmail.com>
2024-06-15 10:56:57 +0300

    Урок 6(Від)

--- weather.py
+++ weather.py
@@ -5,7 +5,7 @@ from kivymd.uix.screen import MDScreen
 from kivymd.uix.backdrop.backdrop import MDCard
 
 import config
-from config import APY_KEY, API_URL
+from config import APY_KEY, API_URL, FORECAST_URL
 import requests as req
 
 
@@ -24,32 +24,48 @@ class WeatherCard(MDCard):
 class MainScreen(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-    def weather_search(self):
-        self.ids.weather_carousel.clear_widgets()
-        #Отримуємо текст з кествого поля міста
-        city = self.ids.text_fell.text.strip().lower()
-        #Робимо налажтування
+    def get_weather_data(self, url, city):
         api_params = {
             'q': city,
             'appid': APY_KEY
 
         }
 
-        #Робимо запит і получаємо відповідь
-        data = req.get(API_URL, api_params)
+        # Робимо запит і получаємо відповідь
+        data = req.get(url, api_params)
         response = data.json()
-        print(response)
+        return response
+    def add_weather_card(self, response):
         descripition = response['weather'][0]['description']
         icon = response['weather'][0]['icon']
         temp = response['main']['temp']
 
         if 'rain' in response:
-            rain = response['rain']['1h']
+            if '1h' in response['rain']:
+
+                rain = response['rain']['1h']
+            else:
+                rain = response['rain']['3h']
+
         else:
             rain = 0
         wind = response['wind']['speed']
         new_card = WeatherCard(descripition, icon, temp, rain, wind)
         self.ids.weather_carousel.add_widget(new_card)
+
+    def weather_search(self):
+        self.ids.weather_carousel.clear_widgets()
+        #Отримуємо текст з кествого поля міста
+        city = self.ids.text_fell.text.strip().lower()
+        #Робимо налажтування
+
+        current_weather = self.get_weather_data(API_URL, city)
+        forecast_weather = self.get_weather_data(FORECAST_URL, city)
+
+        self.add_weather_card(current_weather)
+        for period in forecast_weather['list']:
+            self.add_weather_card(period)
+
 
 class WeatherApp(MDApp):
     def build(self):
